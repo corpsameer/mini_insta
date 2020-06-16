1f00993744546882049e0570725394bcb1d0e3eb
corpsameer <corporate.sameeragarwal@gmail.com>
2020-06-16 12:37:07 +0530

    Changes to enable user to add posts and view post

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,7 +16,7 @@
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror"
-                                    name="name" value="{{ old('name') }}" autocomplete="name" autofocus>
+                                    name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                 @error('name')
                                 <span class="invalid-feedback" role="alert">
@@ -33,7 +33,7 @@
                             <div class="col-md-6">
                                 <input id="username" type="username"
                                     class="form-control @error('username') is-invalid @enderror" name="username"
-                                    value="{{ old('username') }}" autocomplete="username">
+                                    value="{{ old('username') }}" required autocomplete="username">
 
                                 @error('username')
                                 <span class="invalid-feedback" role="alert">
@@ -49,7 +49,7 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" value="{{ old('email') }}" autocomplete="email">
+                                    name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -65,8 +65,8 @@
 
                             <div class="col-md-6">
                                 <input id="password" type="password"
-                                    class="form-control @error('password') is-invalid @enderror" name="password"
-                                    autocomplete="new-password">
+                                    class="form-control @error('password') is-invalid @enderror" required
+                                    name="password" autocomplete="new-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -81,7 +81,7 @@
                                 class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control"
+                                <input id="password-confirm" type="password" class="form-control" required
                                     name="password_confirmation" autocomplete="new-password">
                             </div>
                         </div>
---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fdel27-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fdel27-1.fna.fbcdn.net&_nc_ohc=KyPmDrrmIA0AX84Oymg&oh=52b394af1389faf7985c04eb632f10d5&oe=5F0F6F67"
+                class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a class="btn btn-primary" href="/p/create">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title ?? ''}}</div>
+            <div>{{ $user->profile->description ?? '' }}</div>
+            <div><a href="#">{{ $user->profile->url ?? '' }}</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        @foreach($user->posts as $post)
+        <div class="col-4 pb-4">
+            <a href="/p/{{ $post->id }}"><img src="/storage/{{ $post->image }}" class="w-100"></a>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
