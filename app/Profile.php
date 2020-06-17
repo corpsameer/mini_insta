c9e4e7d0f1d44419caeee1535fcb7541ec5ffcb6
corpsameer <corporate.sameeragarwal@gmail.com>
2020-06-17 11:43:45 +0530

    Implement follow/unfollow functionality using vue

--- app/Profile.php
+++ app/Profile.php
@@ -13,6 +13,11 @@ class Profile extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
     public function profileImage()
     {
         $imagePath = ($this->image) ? $this->image : 'profile/KrkF1bjZWR3gNNv877cde5N1ZWmjrnZMvA9YOs5V.png';
--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -8,7 +8,13 @@
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
-                <h1>{{ $user->username }}</h1>
+                <div class="d-flex align-items-center pb-3">
+                    <div class="h4">
+                        {{ $user->username }}
+                    </div>
+                    <follow-button user-id="{{ $user->id }}" follows="{{ $follows }}"></follow-button>
+                </div>
+
                 @can('update', $user->profile)
                 <a class="btn btn-primary" href="/p/create">Add New Post</a>
                 @endcan
@@ -18,8 +24,8 @@
             @endcan
             <div class="d-flex pt-2">
                 <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
-                <div class="pr-5"><strong>23k</strong> followers</div>
-                <div class="pr-5"><strong>212</strong> following</div>
+                <div class="pr-5"><strong>{{ $user->profile->followers->count() }}</strong> followers</div>
+                <div class="pr-5"><strong>{{ $user->following->count() }}</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
             <div>{{ $user->profile->description }}</div>
